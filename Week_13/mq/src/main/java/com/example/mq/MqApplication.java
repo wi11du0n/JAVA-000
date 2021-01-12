994e9229f33705e54e4af069408d10be6361487c
Will <dqywill@outlook.com>
2021-01-12 23:23:29 +0800

    active mq topic

--- Week_13/mq/src/main/java/com/example/mq/MqApplication.java
+++ Week_13/mq/src/main/java/com/example/mq/MqApplication.java
@@ -13,8 +13,26 @@ public class MqApplication {
     public static void main(String[] args) throws JMSException {
         ApplicationContext ac = SpringApplication.run(MqApplication.class, args);
 
+        listenTopic();
         sendMessage();
         readMessage();
+        System.exit(0);
+    }
+
+    private static void listenTopic() throws JMSException {
+
+        ActiveMQConnectionFactory connectionFactory = getActiveMQConnectionFactory();
+
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination topic = session.createTopic("TOPIC.BAR");
+
+        MessageConsumer topicConsumer1 = session.createConsumer(topic);
+        MessageConsumer topicConsumer2 = session.createConsumer(topic);
+
+        topicConsumer1.setMessageListener(MqApplication::printMessage);
+        topicConsumer2.setMessageListener(MqApplication::printMessage);
     }
 
     private static void readMessage() throws JMSException {
@@ -24,26 +42,31 @@ public class MqApplication {
         connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination destination = session.createQueue("TEST.FOO");
-        MessageProducer producer = session.createProducer(destination);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-
         MessageConsumer consumer = session.createConsumer(destination);
 
         // Wait for a message
         Message message = consumer.receive(1000);
-
-        if (message instanceof TextMessage) {
-            TextMessage textMessage = (TextMessage) message;
-            String text = textMessage.getText();
-            System.out.println("Received: " + text);
-        } else {
-            System.out.println("Received: " + message);
-        }
+        printMessage(message);
 
         consumer.close();
         session.close();
         connection.close();
 
+    }
+
+    private static void printMessage(Message message) {
+        if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            String text = null;
+            try {
+                text = textMessage.getText();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Received: " + text);
+        } else {
+            System.out.println("Received: " + message);
+        }
     }
 
     private static void sendMessage() throws JMSException {
@@ -57,12 +80,18 @@ public class MqApplication {
         producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 
         // Create a messages
-        String text = "Hello world! From: " + Thread.currentThread().getName();
+        String text = "Hello world! From: " + Thread.currentThread().getName() + "-" + System.currentTimeMillis();
         TextMessage message = session.createTextMessage(text);
 
         // Tell the producer to send the message
         System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
         producer.send(message);
+
+        Destination topic = session.createTopic("TOPIC.BAR");
+        MessageProducer topicProducer =  session.createProducer(topic);
+        topicProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        topicProducer.send(message);
+
 
         // Clean up
         session.close();
